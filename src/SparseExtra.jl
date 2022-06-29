fc404f950a2149e14e2c2bb7345b157a472bed70
Sobhan Mohammadpour <sobhan.mohammadpour@umontreal.ca>
2022-06-29 14:15:21 -0400

    stylize

--- src/SparseExtra.jl
+++ src/SparseExtra.jl
@@ -3,6 +3,8 @@ module SparseExtra
 using SparseArrays
 using SparseArrays: AbstractSparseMatrixCSC, AbstractSparseVector
 using SparseArrays: getcolptr, getrowval, getnzval, nonzeroinds
+
+
 include("iternz.jl")
 
 
