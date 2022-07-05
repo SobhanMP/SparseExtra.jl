84dd1e6a87bbd9096985513dc88e83661ac9ba71
Sobhan Mohammadpour <sobhan.mohammadpour@umontreal.ca>
2022-07-05 18:59:10 -0400

    simplify everything

--- src/iternz.jl
+++ src/iternz.jl
@@ -1,16 +1,16 @@
-abstract type SparseIndexIterate end
-
-
-
 
 
 # AbstractSparseMatrixCSC
-struct IterateNZCSC{T<: AbstractSparseMatrixCSC} <: SparseIndexIterate
+struct IterateNZ{N, T}
     m::T
 end
-@inline Base.length(x::IterateNZCSC) = nnz(x.m)
-@inline Base.eltype(::IterateNZCSC{T}) where {Ti, Tv, T <: AbstractSparseMatrixCSC{Tv, Ti}} = Tuple{Tv, Ti, Ti}
-@inline Base.iterate(x::IterateNZCSC, state=(1, 0)) = @inbounds let (j, ind) = state,
+SparseArrays.nnz(x) = length(x)
+iternz(x) = IterateNZ{ndims(x), typeof(x)}(x)
+Base.length(x::IterateNZ) = length(x.m) 
+Base.eltype(x::IterateNZ{N}) where N = Tuple{eltype(x.m), Vararg{Int, N}}
+
+Base.length(x::IterateNZ{2, <:AbstractSparseMatrixCSC}) = nnz(x.m)
+Base.iterate(x::IterateNZ{2, <:AbstractSparseMatrixCSC}, state=(1, 0)) = @inbounds let (j, ind) = state,
     m = x.m
     ind += 1
     while (j < size(m, 2)) && (ind > getcolptr(m)[j + 1] - 1) # skip empty cols or one that have been exhusted
@@ -21,16 +21,8 @@ end
     (getnzval(m)[ind], getrowval(m)[ind], j), (j, ind)
 end
 
-iternz(S::AbstractSparseMatrixCSC) = IterateNZCSC(S)
-
-
-# AbstractSparseVector
-struct IterateSparseVec{T<: AbstractSparseVector} <: SparseIndexIterate
-    m::T
-end
-@inline Base.length(x::IterateSparseVec) = nnz(x.m)
-@inline Base.eltype(::IterateSparseVec{T}) where {Ti, Tv, T <: AbstractSparseVector{Tv, Ti}} = Tuple{Tv, Ti}
-@inline Base.iterate(x::IterateSparseVec, state=0) = @inbounds let m = x.m
+Base.length(x::IterateNZ{1, <:AbstractSparseVector}) = nnz(x.m)
+Base.iterate(x::IterateNZ{1, <:AbstractSparseVector}, state=0) = @inbounds let m = x.m
     state += 1
     if state > nnz(m)
         nothing
@@ -38,88 +30,39 @@ end
         (nonzeros(m)[state], nonzeroinds(m)[state]), state
     end
 end
-iternz(S::AbstractSparseVector) = IterateSparseVec(S)
 
-
-
-# AbstractArray
-struct IterateAbstractArray{T, N, A<:AbstractArray{T, N}}
-    x::A
-end
-
-
-@inline Base.length(x::IterateAbstractArray) = length(x.x)
-@inline Base.eltype(::IterateAbstractArray{T, N}) where {T, N} = Tuple{T, Vararg{Int, N}}
-
-
-@inline Base.iterate(x::IterateAbstractArray) = begin
-    c = CartesianIndices(x.x)
+Base.iterate(x::IterateNZ{N, <:AbstractArray}) where N = begin
+    c = CartesianIndices(x.m)
     i, s = iterate(c)
-    (x.x[i], Tuple(i)...), (c, s)
+    (x.m[i], Tuple(i)...), (c, s)
 end
-
-@inline Base.iterate(x::IterateAbstractArray, state) = @inbounds begin
+Base.iterate(x::IterateNZ{N, <:AbstractArray}, state) where N = @inbounds begin
     res = iterate(state...)
     res === nothing && return nothing
     i, s = res
-    (x.x[i], Tuple(i)...), (state[1], s)
+    (x.m[i], Tuple(i)...), (state[1], s)
 end
 
-iternz(x::A) where {T, N, A<:AbstractArray{T, N}} = IterateAbstractArray{T, N, A}(x)
+Base.length(x::IterateNZ{2, <:Diagonal}) = nnz(x.m.diag)
+Base.iterate(x::IterateNZ{2, <:Diagonal}) = let state = iternz(x.m.diag),
+    a = iterate(state)
 
-
-# Diagonal
-struct IterateDiagonal{T, S}
-    x::S
-end
-Base.length(x::IterateDiagonal) = length(x.x)
-Base.iterate(x::IterateDiagonal) = let a = iterate(x.x)
     a === nothing && return nothing
     (v, i), s = a
-    (v, i, i), s
+    (v, i, i), (state, s)
 end
-Base.iterate(x::IterateDiagonal, state) = let a = iterate(x.x, state)
+Base.iterate(::IterateNZ{2, <:Diagonal}, state) = 
+let a = iterate(state...)
     a === nothing && return nothing
     (v, i), s = a
-    (v, i, i), s
-end
-Base.eltype(::IterateDiagonal{T}) where T = Tuple{T, Int, Int}
-iternz(x::Diagonal{T, <:AbstractVector{T}}) where T = let a = iternz(x.diag)
-    IterateDiagonal{T, typeof(a)}(a)
+    (v, i, i), (state[1], s)
 end
 
 
-# UpperTriangular
-struct IterateUpperTriangular{T, S}
-    x::S
+iternz(x::UpperTriangular) = Iterators.filter(iternz(x.data)) do (_, i, j)
+    i <= j
 end
-Base.eltype(::IterateUpperTriangular{T}) where T = Tuple{T, Int, Int}
-# can't know length
-
-Base.IteratorSize(::Type{IterateUpperTriangular{T, S}}) where {T, S} = Base.SizeUnknown()
-
-Base.iterate(x::IterateUpperTriangular) = let a = iterate(x.x)
-    if a === nothing 
-        nothing
-    else 
-        (_, i, j), state = a
-        if i <= j
-            a
-        else
-            iterate(x, state)
-        end
-    end
-end
-
-Base.iterate(x::IterateUpperTriangular, state) = while true
-    a = iterate(x.x, state)
-    a === nothing && return nothing
-    (_, i, j), state = a
     
-    i <= j && return a
+iternz(x::LowerTriangular) = Iterators.filter(iternz(x.data)) do (_, i, j)
+    i >= j
 end
-
-iternz(x::UpperTriangular{T, <:AbstractMatrix{T}}) where T = let a = iternz(x.data)
-    IterateUpperTriangular{T, typeof(a)}(a)
-end
-
