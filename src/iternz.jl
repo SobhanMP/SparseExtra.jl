14e0106a224d186706eea0748964e73dcf1c4daf
Sobhan Mohammadpour <sobhan.mohammadpour@umontreal.ca>
2022-07-08 15:30:28 -0400

    iternz: specialize stridedmatrix

--- src/iternz.jl
+++ src/iternz.jl
@@ -8,9 +8,11 @@ SparseArrays.nnz(x) = length(x)
 iternz(x) = IterateNZ{ndims(x), typeof(x)}(x)
 Base.length(x::IterateNZ) = length(x.m) 
 Base.eltype(x::IterateNZ{N}) where N = Tuple{eltype(x.m), Vararg{Int, N}}
+@inline skip_col(x::IterateNZ, s) = s
 
 Base.length(x::IterateNZ{2, <:AbstractSparseMatrixCSC}) = nnz(x.m)
-Base.iterate(x::IterateNZ{2, <:AbstractSparseMatrixCSC}, state=(1, 0)) = @inbounds let (j, ind) = state,
+Base.iterate(x::IterateNZ{2, <:AbstractSparseMatrixCSC}, state=(1, 0)) = 
+@inbounds let (j, ind) = state,
     m = x.m
     ind += 1
     while (j < size(m, 2)) && (ind > getcolptr(m)[j + 1] - 1) # skip empty cols or one that have been exhusted
@@ -19,6 +21,16 @@ Base.iterate(x::IterateNZ{2, <:AbstractSparseMatrixCSC}, state=(1, 0)) = @inboun
     (j > size(m, 2) || ind > getcolptr(m)[end] - 1) && return nothing
 
     (getnzval(m)[ind], getrowval(m)[ind], j), (j, ind)
+end
+@inline skip_col(x::IterateNZ{2, <:AbstractSparseMatrixCSC}, state) = 
+@inbounds let (j, ind) = state,
+    m = x.m
+    # skip empty cols
+    while getcolptr(m)[j] > getcolptr(m)[j + 1] - 1
+        j += 1
+    end
+    ind = max(ind, getcolptr(m)[j])
+    j, ind
 end
 
 Base.length(x::IterateNZ{1, <:AbstractSparseVector}) = nnz(x.m)
@@ -34,14 +46,47 @@ end
 Base.iterate(x::IterateNZ{N, <:AbstractArray}) where N = begin
     c = CartesianIndices(x.m)
     i, s = iterate(c)
-    (x.m[i], Tuple(i)...), (c, s)
+    mi, ms = iterate(x.m)
+    (mi, Tuple(i)...), (ms, c, s)
 end
 Base.iterate(x::IterateNZ{N, <:AbstractArray}, state) where N = @inbounds begin
-    res = iterate(state...)
+    ms, ii, is = state
+    res = iterate(ii, is)
     res === nothing && return nothing
+    res1 = iterate(x.m, ms)
+    res1 === nothing && return nothing # shoud not happen
+    mi, nms = res1
     i, s = res
-    (x.m[i], Tuple(i)...), (state[1], s)
+    (mi, Tuple(i)...), (nms, ii, s)
 end
+
+@inline Base.iterate(x::IterateNZ{2, StridedMatrix{T}}) where T = begin
+    ind, i, j = firstindex(x.m), 1, 1
+    (x.m[ind], i, j), (ind, i, j)
+end
+@inline Base.iterate(x::IterateNZ{2, StridedMatrix{T}}, state) where T = @inbounds begin
+    ind, i, j = state
+    i += 1
+    ind += stride(x.m, 1)
+    if i > size(x.m, 1)
+        i = 1
+        j += 1
+        ind = ind - size(x.m, 1) * stride(x.m, 1) + stride(x.m, 2)
+    end
+    if j > size(x.m, 2)
+        return nothing
+    end
+    (x.m[ind], i, j), (ind, i, j)
+end
+
+@inline skip_col(x::IterateNZ{2, <:StridedMatrix}, state) where N =
+@inbounds begin
+    ind, i, j = state
+    ind = ind - (i - 1) * stride(x.m, 1) + stride(x.m, 2)
+    (ind, 1, j + 1)
+end
+
+
 
 Base.length(x::IterateNZ{2, <:Diagonal}) = nnz(x.m.diag)
 Base.iterate(x::IterateNZ{2, <:Diagonal}) = let state = iternz(x.m.diag),
