5816b133d4ba38228d68aa54cb170cafa0926cb9
Sobhan Mohammadpour <sobhan.mohammadpour@umontreal.ca>
2022-07-08 23:20:13 -0400

    add getindex_

--- src/sparse_helpers.jl
+++ src/sparse_helpers.jl
@@ -45,3 +45,15 @@ sparse_like(t::SparseMatrixCSC, z::Vector) =
     SparseMatrixCSC(t.m, t.n, t.colptr, t.rowval, z)
 sparse_like(t::SparseMatrixCSC) =
     SparseMatrixCSC(t.m, t.n, t.colptr, t.rowval, copy(t.nzval))
+
+
+
+function getindex_(A::SparseMatrixCSC, i0::T, i1::T)::T where T
+    @boundscheck checkbounds(A, i0, i1)
+    r1 = STi(A.colptr[i1])
+    r2 = STi(A.colptr[i1+1]-1)
+    (r1 > r2) && return zero(T)
+    r1 = searchsortedfirst(A.rowval, i0, r1, r2, Base.Forward)
+    @boundscheck !((r1 > r2) || (rowvals(A)[r1] != i0))
+    return r1
+end
