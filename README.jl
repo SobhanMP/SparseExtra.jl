237cb213845b984d52556701ebd52dbb31fd809f
Sobhan Mohammadpour <sobhan.mohammadpour@umontreal.ca>
2022-07-26 20:22:07 -0400

    update Readme

--- README.jl
+++
@@ -1,4 +0,0 @@
-# ![example workflow](https://github.com/sobhanmp/SparseExtra.jl/actions/workflows/ci.yml/badge.svg)
-# [![codecov](https://codecov.io/gh/SobhanMP/SparseExtra.jl/branch/main/graph/badge.svg?token=MzXyDmfScn)](https://codecov.io/gh/SobhanMP/SparseExtra.jl)
-
-# Collections of mostly sparse stuff developed by me.
